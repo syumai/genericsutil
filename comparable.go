cd39e9713b4a39c7082e79670f575639f745c44e
syumai <syumai@gmail.com>
2022-02-24 10:35:12 +0900

    omit type parameter name in IsComparable

--- comparable.go
+++ comparable.go
@@ -1,4 +1,4 @@
 package genericsutil
 
 // IsComparable checks if given type argument implements comparable interface in compile time.
-type IsComparable[T comparable] struct{}
+type IsComparable[_ comparable] struct{}
